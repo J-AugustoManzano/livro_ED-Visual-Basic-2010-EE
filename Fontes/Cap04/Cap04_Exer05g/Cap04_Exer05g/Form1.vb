0b7ce1622ad5958b18505128dbf4d401e3a6f0b0
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-09 11:39:14 -0300

    Add files via upload

---
+++ Fontes/Cap04/Cap04_Exer05g/Cap04_Exer05g/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        Dim N, I, ATUAL, ANTERIOR, PROXIMO As Integer
+
+        N = TextBox1.Text
+
+        ANTERIOR = 0
+        ATUAL = 1
+
+        For I = 1 To N Step 1
+            PROXIMO = ATUAL + ANTERIOR
+            ANTERIOR = ATUAL
+            ATUAL = PROXIMO
+        Next
+
+        Label1.Text = ANTERIOR
+
+    End Sub
+End Class
