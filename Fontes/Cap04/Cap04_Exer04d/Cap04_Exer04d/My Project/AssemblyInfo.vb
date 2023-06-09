6997edfe78f8f553fd923dbf2241319b9a419a02
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-09 11:34:57 -0300

    Add files via upload

---
+++ Fontes/Cap04/Cap04_Exer04d/Cap04_Exer04d/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Cap04_Exer04d")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("Cap04_Exer04d")> 
+<Assembly: AssemblyCopyright("Copyright ©  2010")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("19cdc42c-e7b0-48e8-8c8d-3bce802154c2")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ Fontes/Cap04/Cap04_Exer04f/Cap04_Exer04f/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        Dim S, QUAD, I As Integer
+
+        S = 0
+
+        I = 2
+        Do
+            QUAD = I ^ 2
+            S = S + QUAD
+            If (I >= 5) Then
+                Exit Do
+            End If
+            I = I + 1
+        Loop
+
+        Label1.Text = S
+
+    End Sub
+End Class
