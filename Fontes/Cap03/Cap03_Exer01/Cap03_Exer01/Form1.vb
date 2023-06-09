0cc2b2b4125dafba2411749371e3060686fbc9d5
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-09 11:16:47 -0300

    Add files via upload

---
+++ Fontes/Cap03/Cap03_Exer01/Cap03_Exer01/Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        Dim A, B, C, X As Integer
+
+        A = TextBox1.Text
+        B = TextBox2.Text
+        C = TextBox3.Text
+
+        If (A > B) Then
+            X = A
+            A = B
+            B = X
+        End If
+
+        If (A > C) Then
+            X = A
+            A = C
+            C = X
+        End If
+
+        If (B > C) Then
+            X = B
+            B = C
+            C = X
+        End If
+
+        Label1.Text = A
+        Label2.Text = B
+        Label3.Text = C
+
+    End Sub
+End Class
---
+++ Fontes/Cap03/Cap03_Exer02/Cap03_Exer02/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        Dim A, B, C, DELTA, X1, X2 As Single
+
+        A = TextBox1.Text
+        B = TextBox2.Text
+        C = TextBox3.Text
+
+        If (A <> 0 And B <> 0 And C <> 0) Then
+            DELTA = B ^ 2 - 4 * A * C
+            If (DELTA = 0) Then
+                X1 = -B / (2 * A)
+                Label1.Text = X1
+            Else
+                If (DELTA > 0) Then
+                    X1 = (-B + DELTA ^ (1 / 2)) / (2 * A)
+                    X2 = (-B - DELTA ^ (1 / 2)) / (2 * A)
+                    Label1.Text = Str(X1) & " " & Str(X2)
+                Else
+                    Label1.Text = "Não existem raízes"
+                End If
+            End If
+        Else
+            Label1.Text = "Cálculo impossível"
+        End If
+
+    End Sub
+End Class
