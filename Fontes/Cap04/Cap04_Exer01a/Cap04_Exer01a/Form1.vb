a4b8a093dcff6aa49509faf5655d8ccf9b9f1474
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-09 11:20:18 -0300

    Add files via upload

---
+++ Fontes/Cap04/Cap04_Exer01a/Cap04_Exer01a/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        Dim S, I, R As Integer
+
+        S = 0
+
+        I = 0
+        Do While (I <= 20)
+            R = I Mod 2
+            If (R <> 0) Then ' Não use R = 1 como condição
+                S = S + I
+            End If
+            I = I + 1
+        Loop
+
+        Label1.Text = S
+
+    End Sub
+End Class
