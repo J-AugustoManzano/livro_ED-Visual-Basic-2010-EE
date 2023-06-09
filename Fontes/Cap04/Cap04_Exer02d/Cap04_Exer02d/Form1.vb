5a9cf0a863e751ebabd983a8a26c97577eea0470
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-09 11:24:51 -0300

    Add files via upload

---
+++ Fontes/Cap04/Cap04_Exer02d/Cap04_Exer02d/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        Dim S, I As Integer
+
+        S = 0
+
+        I = 1
+        Do
+            S = S + I
+            I = I + 1
+        Loop While (I <= 100)
+
+        Label1.Text = S
+
+    End Sub
+End Class
