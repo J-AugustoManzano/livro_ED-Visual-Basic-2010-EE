ad7c517dd22e659222146ab60cf03b42ad99c6bb
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-09 11:13:25 -0300

    Add files via upload

---
+++ Fontes/Cap02/Cap02_Exer02/Cap02_Exer02/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        Dim TEMPO, VELOCIDADE, DISTANCIA, LITROS_USADOS As Single
+
+        TEMPO = TextBox1.Text
+        VELOCIDADE = TextBox2.Text
+
+        DISTANCIA = TEMPO * VELOCIDADE
+        LITROS_USADOS = DISTANCIA / 12
+
+        Label1.Text = TEMPO
+        Label2.Text = VELOCIDADE
+        Label3.Text = DISTANCIA
+        Label4.Text = LITROS_USADOS
+
+    End Sub
+End Class
---
+++ Fontes/Cap02/Cap02_Exer03/Cap02_Exer03/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        Dim F, C As Single
+        C = TextBox1.Text
+        F = (9 * C + 160) / 5
+        Label1.Text = F
+
+    End Sub
+End Class
